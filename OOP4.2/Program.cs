4ad38a0d46e9cdab24f06074da1310044704eb57
POSTOVALOVDENIS <67200908+POSTOVALOVDENIS@users.noreply.github.com>
2020-06-27 22:38:06 +0500

    Добавьте файлы проекта.

---
+++ OOP4.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace OOP4._2
+{
+    class Book
+    {
+        private int diapazon;
+        public int Year
+        {
+            get
+            {
+                return diapazon;
+            }
+            set
+            {
+                if (value >= 1940 && value <= 2020)
+                    Console.WriteLine("Год выпуска в диапозоне");
+                else
+                    Console.WriteLine("Год выпуска не в диапозоне от 1940 до 2020, повторите попытку");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите год создания книги в диапозоне от 1940 до 2020");
+            int godknigi = Convert.ToInt32(Console.ReadLine());
+            Book Book = new Book();
+            Book.Year = godknigi;
+            Console.ReadLine();
+        }
+    }
+}
